ebff2f5f25eca3c4d28ec24ead99bb2558af30b8
Anthony Keba <aclkeba@gmail.com>
2017-01-30 11:38:50 -0800

    added new icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,12 @@ function Home() {
       </div>
       <br />
       <div className="fixed-action-btn">
-        <a className="btn-floating btn-large red"><i className="large material-icons">social_media</i></a>
+        <a className="btn-floating btn-large red"><i className="mdi mdi-share-variant">social_media</i></a>
         <ul>
-          <li><a className="btn-floating red"><i className="material-icons">insert_chart</i></a></li>
-          <li><a className="btn-floating yellow darken-1"><i className="material-icons">insert_chart</i></a></li>
-          <li><a className="btn-floating green"><i className="material-icons">insert_chart</i></a></li>
-          <li><a className="btn-floating blue"><i className="material-icons">insert_chart</i></a></li>
+          <li><a className="btn-floating yellow darken-1" href="https://github.com/CatLoverKid"><i className="mdi mdi-github-circle"></i></a></li>
+          <li><a className="btn-floating red" href="http://www.aclkeba@gmail.com"><i className="mdi mdi-gmail">insert_chart</i></a></li>
+          <li><a className="btn-floating green" href="http://www.linkedin.com/in/anthony-keba-430b6762"><i className="mdi mdi-linkedin-box">insert_chart</i></a></li>
+          <li><a className="btn-floating blue" href="https://www.facebook.com/CatLoverKid"><i className="mdi mdi-facebook-box">insert_chart</i></a></li>
         </ul>
       </div>
     </div>
