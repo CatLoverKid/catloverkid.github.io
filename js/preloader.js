d07c84cb46b52c66610d76c4c33accad599f04e4
Anthony Keba <Anthony.Keba@FiveTalent.com>
2018-02-13 09:31:17 -0800

    added new Default Theme, included Adobe Typekit

--- js/preloader.js
+++ js/preloader.js
@@ -3,45 +3,64 @@
 
     let vaporwave = {
             phoneRingAudio: new Audio('assets/phone.mp3'),
-            link: document.createElement( "link" ),
+            cssLink: document.createElement( "link" ),
             init: () => {
-                vaporwave.link.href = "css/synthwave.css";
-                vaporwave.link.type = "text/css";
-                vaporwave.link.rel = "stylesheet";
-                vaporwave.link.media = "screen,print";
+                vaporwave.cssLink.href = "css/synthwave.css";
+                vaporwave.cssLink.type = "text/css";
+                vaporwave.cssLink.rel = "stylesheet";
+                vaporwave.cssLink.media = "screen,print";
 
                 $("#masterContainer").empty().append(
-                    $("<div/>").addClass("grid").append(
-                        
-                    )
-
-
-
-                )
-                    $("<div/>").addClass("phoneContainer").append(
+                    $("<div/>").append(
                         $("<img/>").attr({
                             src: '../assets/phoneBaseTEMP.png',
                             class: 'introPhone'
                         })
+                    ).append(
+                        $("<h1/>").append("S Y N T H W A V E")
                     )
-                ).append(
-                    $("<h1/>").append("S Y N T H W A V E")
                 )
-                document.getElementsByTagName( "head" )[0].appendChild( vaporwave.link );
+                document.getElementsByTagName( "head" )[0].appendChild( vaporwave.cssLink );
                 vaporwave.phoneRingAudio.loop = true;
-                vaporwave.phoneRingAudio.play();
+                //vaporwave.phoneRingAudio.play();
             }
         };
 
         let defaultTheme = {
+            cssLink: document.createElement( "link" ),
             init: () => {
+                defaultTheme.cssLink.href = "css/defaultTheme.css";
+                defaultTheme.cssLink.type = "text/css";
+                defaultTheme.cssLink.rel = "stylesheet";
+                defaultTheme.cssLink.media = "screen,print";
                 $("#masterContainer").empty().append(
                     $("<div/>").addClass("blackBackground").append(
-                        $("<p/>").append("Test")
+                        $("<div/>").addClass("gridWrapper").append(
+                            $("<div/>").addClass("gridBox BoxA").append(
+                                "Test1"
+                            )
+                        ).append(
+                            $("<div/>").addClass("gridBox BoxB").append(
+                                "Test2"
+                            )
+                        ).append(
+                            $("<div/>").addClass("gridBox BoxC").append(
+                                "Test3"
+                            )
+                        ).append(
+                            $("<div/>").addClass("gridBox BoxD").append(
+                                "Test4"
+                            )
+                        ).append(
+                            $("<div/>").addClass("gridBox BoxE").append(
+                                "Test5"
+                            )
+                        )
                     )
                 )
+                document.getElementsByTagName( "head" )[0].appendChild( defaultTheme.cssLink );
             }
         }
 
-    window.preloadingModule = vaporwave;
+    window.preloadingModule = defaultTheme;
 })();
