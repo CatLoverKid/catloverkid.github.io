2f5cf28c1282cf457849d01a334622d9dc94b857
Anthony Keba <Anthony.Keba@FiveTalent.com>
2018-02-14 21:38:42 -0800

    removed default.css, renamed to the more verbose default-theme.css

--- js/preloader.js
+++ js/preloader.js
@@ -1,14 +1,14 @@
 "use strict";
 (() => {
 
-    let vaporwave = {
+    let synthwave = {
             phoneRingAudio: new Audio('assets/phone.mp3'),
             cssLink: document.createElement( "link" ),
             init: () => {
-                vaporwave.cssLink.href = "css/synthwave.css";
-                vaporwave.cssLink.type = "text/css";
-                vaporwave.cssLink.rel = "stylesheet";
-                vaporwave.cssLink.media = "screen,print";
+                synthwave.cssLink.href = "css/synthwave.css";
+                synthwave.cssLink.type = "text/css";
+                synthwave.cssLink.rel = "stylesheet";
+                synthwave.cssLink.media = "screen,print";
 
                 $("#masterContainer").empty().append(
                     $("<div/>").append(
@@ -20,9 +20,9 @@
                         $("<h1/>").append("S Y N T H W A V E")
                     )
                 )
-                document.getElementsByTagName( "head" )[0].appendChild( vaporwave.cssLink );
-                vaporwave.phoneRingAudio.loop = true;
-                //vaporwave.phoneRingAudio.play();
+                document.getElementsByTagName( "head" )[0].appendChild( synthwave.cssLink );
+                synthwave.phoneRingAudio.loop = true;
+                //synthwave.phoneRingAudio.play();
             }
         };
 
@@ -34,26 +34,28 @@
                 defaultTheme.cssLink.rel = "stylesheet";
                 defaultTheme.cssLink.media = "screen,print";
                 $("#masterContainer").empty().append(
-                    $("<div/>").addClass("blackBackground").append(
-                        $("<div/>").addClass("gridWrapper").append(
-                            $("<div/>").addClass("gridBox BoxA").append(
-                                "Test1"
-                            )
-                        ).append(
-                            $("<div/>").addClass("gridBox BoxB").append(
-                                "Test2"
-                            )
-                        ).append(
-                            $("<div/>").addClass("gridBox BoxC").append(
-                                "Test3"
-                            )
-                        ).append(
-                            $("<div/>").addClass("gridBox BoxD").append(
-                                "Test4"
-                            )
-                        ).append(
-                            $("<div/>").addClass("gridBox BoxE").append(
-                                "Test5"
+                    $("<div/>").addClass("row").append(
+                        $("<div/>").addClass("col-lg-12").append(
+                            $("<div/>").addClass("gridWrapper").append(
+                                $("<div/>").addClass("gridBox BoxA").append(
+                                    "Test1"
+                                )
+                            ).append(
+                                $("<div/>").addClass("gridBox BoxB").append(
+                                    "Test2"
+                                )
+                            ).append(
+                                $("<div/>").addClass("gridBox BoxC").append(
+                                    "Test3"
+                                )
+                            ).append(
+                                $("<div/>").addClass("gridBox BoxD").append(
+                                    "Test4"
+                                )
+                            ).append(
+                                $("<div/>").addClass("gridBox BoxE").append(
+                                    "Test5"
+                                )
                             )
                         )
                     )
